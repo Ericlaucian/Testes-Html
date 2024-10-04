95dae92b95fb095194286a323bd12368e9e5bbbb
Eric_Laucian <105080431+Ericlaucian@users.noreply.github.com>
2024-10-04 11:29:04 -0300

    Mudei o nome e agora os cursos vão para uma lista

---
+++ funcoesTela.js
@@ -0,0 +1,71 @@
+
+let idade;
+let cursos = [];
+let posicaoLista;
+
+function Enviar(){
+    let nome = document.getElementById("nome").value;
+    let linkedIN_url = document.getElementById("linkedIn").value;
+
+    calcularIdade();
+
+    //document.getElementById("print").textContent = nome + linkedIN_url + idade + "\n" + cursos;
+}
+
+
+function calcularIdade(){
+    const nascimento = document.getElementById("dtNascimento").value;
+
+    let dtNascimento = new Date(nascimento);
+    let hoje = new Date;
+
+    if(hoje.getMonth() > dtNascimento.getMonth() && hoje.getDay() > dtNascimento.getDay()){
+        idade = hoje.getFullYear() - dtNascimento.getFullYear() - 1;
+    } else{
+        idade = hoje.getFullYear() - dtNascimento.getFullYear();
+    }
+
+}
+
+function TI(){
+    const verificar = document.getElementById("TI");
+    if(verificar.checked){
+        cursos.unshift(document.getElementById("TI").value);
+    } else{
+        cursos = cursos.filter(function(cursos){return cursos !== "TI"});
+    }
+    document.getElementById("print").textContent = cursos;
+}
+
+function Mecatronica(){
+    const verificar = document.getElementById("Mecatronica");
+    if(verificar.checked){
+        cursos.unshift(document.getElementById("Mecatronica").value);
+    } else{
+        cursos = cursos.filter(function(cursos){return cursos !== "Mecatronica"});
+    }
+    document.getElementById("print").textContent = cursos;
+}
+
+function Automacao(){
+    const verificar = document.getElementById("Automacao");
+    if(verificar.checked){
+        cursos.unshift(document.getElementById("Automacao").value);
+    } else{
+        cursos = cursos.filter(function(cursos){return cursos !== "Automacao"});
+    }
+    document.getElementById("print").textContent = cursos;
+}
+
+function ADM(){
+    const verificar = document.getElementById("ADM");
+    if(verificar.checked){
+        cursos.unshift(document.getElementById("ADM").value);
+    } else{
+        cursos = cursos.filter(function(cursos){return cursos !== "ADM"});
+    }
+    document.getElementById("print").textContent = cursos;
+}
+
+
+
